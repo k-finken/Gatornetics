5d7fc8a9e50fe3b139f1ff026847fdbb10e9b00e
cjmartinez217 <cjose.martinez@ufl.edu>
2022-11-01 15:24:56 -0400

    structural additions to sign on page

--- pages/signUp.js
+++ pages/signUp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,6 +9,21 @@ export default function Home() {
       <div className='mt-10 w-full flex justify-center items-center'>
         <h1 className="text-3xl font-bold underline">Signup Page</h1> 
       </div>
+        <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+            <button type="submit">Login</button>
+        </div>
+        <div className="container">
+            <li className="text-red-600">
+                <Link href="/">Cancel</Link>
+            </li>
+            <span className="psw">Forgot <a href="#">password?</a></span>
+        </div>
     </>
   )
 }
