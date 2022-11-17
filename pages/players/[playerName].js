a60be1ece796c8462ac7f4bf6dd362093dffc2c3
cjmartinez217 <cjmartinez217@gmail.com>
2022-11-17 08:55:32 -0500

    player cards WIP

--- pages/players/[playerName].js
+++ pages/players/[playerName].js
@@ -21,32 +21,39 @@ const PlayerDetails = () => {
         <>
             <Navbar/>
             <div>
-                <div className="w-30 left flex items-left mb-2 justify-left">
-                    <div className="flex h-28 items-right justify-start">
-                        <img className="flex mx-20 bg-gray-400" src="https://a.espncdn.com/combiner/i?img=/i/headshots/college-football/players/full/4429084.png" width="140" height="140"/>
-                        <div className="flex flex-col">
-                            <div className="flex flex-col items-left text-gray-600 text-s">
-                                <div className="flex items-end">Name:
-                                    <h3 className="text-s">{player}</h3>
-                                </div>
-                                <div className="flex items-end">Height:
-                                    <h3 className="text-s">6'4"</h3>
-                                </div>
-                                <div className="flex items-end">Weight:
-                                    <h3 className="text-s">232 Ibs</h3>
-                                </div>
-                                <div className="flex items-end">Position:
-                                    <h3 className="text-s">QB</h3>
-                                </div>
-                                <div className="flex items-end">Year:
-                                    <h3 className="text-s">Sophomore</h3>
-                                </div>
-                                <div className="flex items-end">Team:
-                                    <h3 className="text-s">Florida Gators</h3>
-                                </div>
+                <div className="flex">
+                    <img className="flex h-200 mx-20 bg-gray-400" src="https://a.espncdn.com/combiner/i?img=/i/headshots/college-football/players/full/4429084.png" width="350" height="400"/>
+                    <div className="flex flex-col mx-20 justify-right">
+                        <div className="flex flex-col items-left text-gray-600 text-s">
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Name: </h3>
+                                <h3 className="text-s"><b>{player}</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Height: </h3>
+                                <h3 className="text-xl"><b>6'4"</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Weight:</h3>
+                                <h3 className="text-xl"><b>232 Ibs</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Position:</h3>
+                                <h3 className="text-xl"><b>QB</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Year: </h3>
+                                <h3 className="text-xl"><b> Sophomore</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Team:</h3>
+                                <h3 className="text-xl"><b>Florida Gators</b></h3>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div>
+
                 </div>
             </div>
             <Footer/>
