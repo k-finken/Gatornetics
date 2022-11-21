618c34e877356d80d0aac3069b05d6d9122438b1
k-finken <kfinken@ufl.edu>
2022-11-21 00:34:25 -0500

    fix broken ml page
    title

--- pages/machine-learning.js
+++ pages/machine-learning.js
@@ -1,50 +1,14 @@
 import React from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-export default function Machinelearning({players}) {
-
-  const dataGraph = {
-    labels: ["Florida", "Texas A&M", "Ohio State", "Georgia"],
-    datasets: [
-      {
-        label: "This years national champion",
-        data: [85,5,5,5],
-        backgroundColor: [
-          "rgb(173, 216, 230)",
-          "rgb(144, 238, 144)",
-        ],
-        hoverOffset: 2,
-      },
-    ],
-  };
-
-
+export default function MachineLearning() {
   return (
     <Layout>
       <div className='mt-10 w-full flex justify-center items-center'>
-        <div className="w-full flex justify-center items-center mt-10">
-        <Bar data={dataGraph}></Bar>
-        </div>
+        <h1 className="text-3xl font-bold text-gray-100">Machine Learning Page</h1> 
       </div>
+      <div className="h-96"></div>
+      <div className="h-36"></div>
     </Layout>
   )
 }
