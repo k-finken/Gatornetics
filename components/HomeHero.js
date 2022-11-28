178e45af8a25439ef2962a043575eb9f2691edac
masongreen30 <73558998+masongreen30@users.noreply.github.com>
2022-11-28 11:42:21 -0500

    Update HomeHero.js
    Graph does not disappear for small screens. It will appear under the text.

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -59,8 +59,8 @@ function Index({foundPlayer}) {
 
     return (
         <section className="">
-            <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-                <div className="ml-10 mr-auto place-self-center lg:col-span-7">
+            <div className="flex flex-wrap justify-center max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+                <div className="ml-4 mr-5 sm:mr-10 md:mr-16 lg:mr-20 place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white text-gray-200">Welcome to Gatornetics</h1>
                     <p className="max-w-2xl mb-6 font-light text-gray-200 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From data visualization to in-depth machine learning models we provide all the tools you need.</p>
                     <p href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg transition focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
@@ -71,7 +71,7 @@ function Index({foundPlayer}) {
                         <a className="animate-bounce inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-200 border hover:text-gray-700 transition border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Signup</a>
                     </Link>
                 </div>
-                <div className="mr-10 hidden lg:mt-0 lg:col-span-5 lg:flex"> 
+                <div className="mr-10 mt-10 sm:mt-0 lg:col-span-5 lg:flex max-w-[300px] max-h-[300px] sm:min-w-[400px] sm:min-h-[400px]"> 
                     <Pie data={dataPie} options={options}></Pie>
                 </div>
             </div>
