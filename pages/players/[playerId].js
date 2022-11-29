60aa72c9882a136216435dd437f83d03fc4a1fd9
k-finken <kfinken@ufl.edu>
2022-11-28 23:42:41 -0500

    Graphs
    Adding graphs

--- pages/players/[playerId].js
+++ pages/players/[playerId].js
@@ -1,6 +1,27 @@
 import { prisma, PrismaClient } from '@prisma/client';
 import Layout from '../../components/Layout';
+import React, { useState } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+);
 
 //this function generates all the paths that the dynamic query can take
 export async function getStaticPaths() {
@@ -8,7 +29,7 @@ export async function getStaticPaths() {
     const players = await prisma.players.findMany()
 
     const paths = players.map((player) => ({
-        params: {playerId: player.id.toString()}
+        params: { playerId: player.id.toString() }
     }))
 
     return { paths, fallback: false }
@@ -34,20 +55,218 @@ export async function getStaticProps(context) {
 }
 
 const PlayerDetails = ({ playerData }) => {
+    const [graphNumber, setGraphNumber] = useState(1);
+
+    const playerPosStat1 = playerData.posStat1[0].replace('[', '').replace(']', '').split(', ');
+    const playerGraphStat1 = playerPosStat1.map(Number);
+    const playerPosStat2 = playerData.posStat2[0].replace('[', '').replace(']', '').split(', ');
+    const playerGraphStat2 = playerPosStat2.map(Number);
+
+    const labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12'];
+
+    let label1Value = "";
+    let title1Value = "";
+
+    switch(playerData.position){
+        case "RB": 
+            label1Value = "Rushing Yards";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Rushing Yards";
+            break;
+        case "QB":
+            label1Value = "Passing Yards";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Passing Yards";
+            break;
+        case "LB":
+            label1Value = "Solo Tackles";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Solo Tackles";
+            break;
+        case "DL":
+            label1Value = "Solo Tackles";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Solo Tackles";
+            break;
+        case "DB":
+            label1Value = "Solo Tackles";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Solo Tackles";
+            break;
+        case "PK":
+            label1Value = "Field Goal Percentage";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Field Goal Percentage";
+            break;
+        case "S":
+            label1Value = "Solo Tackles";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Solo Tackles";
+            break;
+        case "WR":
+            label1Value = "Receiving Yards";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Receiving Yards";
+            break;
+        case "TE":
+            label1Value = "Receiving Yards";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Receiving Yards";
+            break;
+        case "DE":
+            label1Value = "Solo Tackles";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Solo Tackles";
+            break;
+        case "DT":
+            label1Value = "Solo Tackles";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Solo Tackles";
+            break;
+        case "P":
+            label1Value = "Yards Per Point";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Yards Per Point";
+            break;
+        case "CB":
+            label1Value = "Interceptions";
+            title1Value = playerData.firstName + " " + playerData.lastName + "- Weekly Interceptions";
+            break;
+    }
+
+    let label2Value = "";
+    let title2Value = "";
+
+    switch(playerData.position){
+        case "RB": 
+            label2Value = "Rushing Touchdowns";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Weekly Rushing TouchDowns";
+            break;
+        case "QB":
+            label2Value = "Passing Touchdowns";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Weekly Passing TouchDowns";
+            break;
+        case "LB":
+            label2Value = "Total Tackles";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Total Tackles";
+            break;
+        case "DL":
+            label2Value = "Total Tackles";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Total Tackles";
+            break;
+        case "DB":
+            label2Value = "Total Tackles";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Total Tackles";
+            break;
+        case "PK":
+            label2Value = "Points From Kicks";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Total Points From Kicks";
+            break;
+        case "S":
+            label2Value = "Total Tackles";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Total Tackles";
+            break;
+        case "WR":
+            label2Value = "Receiving Touchdowns";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Weekly Receiving Touchdowns";
+            break;
+        case "TE":
+            label2Value = "Receptions";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Weekly Receptions";
+            break;
+        case "DE":
+            label2Value = "Sacks";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Weekly Sacks";
+            break;
+        case "DT":
+            label2Value = "Total Tackles";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Total Tackles";
+            break;
+        case "P":
+            label2Value = "Punting Yards";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Weekly Punting Yards";
+            break;
+        case "CB":
+            label2Value = "Passes Defended";
+            title2Value = playerData.firstName + " " + playerData.lastName + "- Weekly Passes Defended";
+            break;
+    }
+
+
+    const playerGraphData1 = {
+        labels,
+        datasets: [
+            {
+                label: label1Value,
+                data: playerGraphStat1,
+                borderColor: 'rgb(0, 94, 184)',
+                backgroundColor: 'rgba(0, 94, 184)',
+            },
+        ],
+    };
+
+    const playerGraphData2 = {
+        labels,
+        datasets: [
+            {
+                label: label2Value,
+                data: playerGraphStat2,
+                borderColor: 'rgb(0, 94, 184)',
+                backgroundColor: 'rgba(0, 94, 184)',
+            },
+        ],
+    };
+
+    const options1 = {
+        responsive: true,
+        scales: {
+            y: {
+              ticks: { color: "#D3D3D3", beginAtZero: true }
+            },
+            x: {
+              ticks: { color: 'white', beginAtZero: true }
+            }
+          },
+        plugins: {
+            title: {
+                display: true,
+                text: title1Value,
+                color: 'white',
+                font: {
+                    size: 34,
+                    family: "system-ui",
+                    weight: "700"
+                }
+            },
+        },
+    };
+
+    const options2 = {
+        responsive: true,
+        scales: {
+            y: {
+              ticks: { color: "#D3D3D3", beginAtZero: true }
+            },
+            x: {
+              ticks: { color: 'white', beginAtZero: true }
+            }
+          },
+        plugins: {
+            title: {
+                display: true,
+                text: title2Value,
+                color: 'white',
+                font: {
+                    size: 34,
+                    family: "system-ui",
+                    weight: "700"
+                }
+            },
+        },
+    };
+
     return (
         <Layout>
             <div>
                 <div className="flex flex-wrap justify-center mt-5">
-                    <img className="flex h-200 mx-20 bg-gray-400 rounded-lg" src="https://a.espncdn.com/combiner/i?img=/i/headshots/college-football/players/full/4429084.png" width="350" height="400" />
+                    <img className="flex h-200 mx-20 rounded-lg" src={playerData.pPic} width="350" height="400" />
                     <div className="flex flex-col mx-20 justify-right">
-                        <div className="m-auto flex flex-col items-left text-gray-600 text-s">
+                        <div className="m-auto flex flex-col items-left text-white text-s">
                             <div className="flex items-end">
                                 <h3 className="text-xl">Name:</h3>
                                 <h3 className="text-2xl ml-5 sm:ml-10"><b>{playerData.firstName} {playerData.lastName}</b></h3>
                             </div>
                             <div className="flex items-end">
                                 <h3 className="text-xl">Height:</h3>
-                                <h3 className="text-2xl ml-4 sm:ml-9"><b>{Math.floor(playerData.height/12)}ft {playerData.height%12}in</b></h3>
+                                <h3 className="text-2xl ml-4 sm:ml-9"><b>{Math.floor(playerData.height / 12)}ft {playerData.height % 12}in</b></h3>
                             </div>
                             <div className="flex items-end">
                                 <h3 className="text-xl">Weight:</h3>
@@ -69,13 +288,18 @@ const PlayerDetails = ({ playerData }) => {
                     </div>
                 </div>
                 <div className="flex justify-center items-center mt-10 sm:mt-15 md:mt-18 lg:mt-20">
-                    <button className="hover:text-white hover:bg-gray-600 lg:text-2xl bg-gray-300 transition duration-150 ease-in-out focus:outline-white focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700 focus:ring-white rounded border border-white text-black px-4 sm:px-20 py-1 sm:py-3 text-sm">Passing Yards</button>
-                    <button className="hover:text-white hover:bg-gray-600 lg:text-2xl bg-gray-300 transition duration-150 ease-in-out focus:outline-white focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700 focus:ring-white rounded border border-white text-black px-4 sm:px-20 py-1 sm:py-3 text-sm ml-6 sm:ml-10">Rushing Yards</button>
+                    {/* <button className="hover:text-white hover:bg-gray-600 lg:text-2xl bg-gray-300 transition duration-150 ease-in-out focus:outline-white focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700 focus:ring-white rounded border border-white text-black px-4 sm:px-20 py-1 sm:py-3 text-sm">Passing Yards</button>
+                    <button className="hover:text-white hover:bg-gray-600 lg:text-2xl bg-gray-300 transition duration-150 ease-in-out focus:outline-white focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700 focus:ring-white rounded border border-white text-black px-4 sm:px-20 py-1 sm:py-3 text-sm ml-6 sm:ml-10">Rushing Yards</button> */}
                 </div>
                 <div>
-                    <h1 className="mt-10 flex justify-center text-3xl font-bold">Charts go here</h1>
+                    <h1 className="mt-10 flex justify-center text-3xl font-bold"></h1>
                 </div>
-                <div className="mt-20 w-full h-96 bg-slate-500 "/>
+                <div className='mx-96'>
+                    <Line options={options1} data={playerGraphData1} />
+                </div>
+                <div className='mt-20 mx-96'>
+                    <Line options={options2} data={playerGraphData2} />
+                </div>
             </div>
         </Layout>
     );
--- pages/teams/[teamId].js
+++ pages/teams/[teamId].js
@@ -1,6 +1,29 @@
 import { useRouter } from 'next/router';
 import { prisma, PrismaClient } from '@prisma/client';
 import Layout from '../../components/Layout'
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    LineElement,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { Scatter } from 'react-chartjs-2';
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    Title,
+    Tooltip,
+);
 
 //this function generates all the paths that the dynamic query can take
 export async function getStaticPaths() {
@@ -8,7 +31,7 @@ export async function getStaticPaths() {
     const teams = await prisma.teams.findMany()
 
     const paths = teams.map((team) => ({
-        params: {teamId: team.id.toString()}
+        params: { teamId: team.id.toString() }
     }))
 
     return { paths, fallback: false }
@@ -28,20 +51,97 @@ export async function getStaticProps(context) {
         }
     })
 
+    const divisionTeamData = await prisma.teams.findMany({
+        where: {
+            division: teamData.division,
+        }
+    });
+
+    const allTeamData = await prisma.teams.findMany();
+
     return {
-        props: { teamData }
+        props: { teamData, divisionTeamData, allTeamData }
     }
 }
 
-const TeamDetails = ({ teamData }) => {
+const TeamDetails = ({ teamData, divisionTeamData, allTeamData }) => {
+
+    let teamIndex = 0;
+    let Index = 0;
+    divisionTeamData.forEach(team => {
+            if (team.school == teamData.school) {
+                teamIndex = Index;
+            }
+            Index++;
+    });
+
+    const options = {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Talent Scores By Team',
+                color: 'white',
+                font: {
+                    size: 34,
+                    family: "system-ui",
+                    weight: "700"
+                }
+            },
+            labels: {
+                fontColor: 'white'
+            }
+        },
+        scales: {
+            y: {
+                ticks: { color: "#D3D3D3", beginAtZero: true }
+            },
+            x: {
+                ticks: { color: 'white', beginAtZero: true }
+            }
+        }
+    };
+
+    const labels = [];
+    divisionTeamData.forEach(team => {
+        labels.push(team.school);
+    });
+
+
+    const talentScores = [];
+    divisionTeamData.forEach(team => {
+        talentScores.push(team.talentScore);
+    });
+
+    //create background colors
+    let bgColors = [];
+    for (let i = 0; i < divisionTeamData.length; i++) {
+        if (i == teamIndex) {
+            bgColors.push(teamData.color);
+        }
+        else {
+            bgColors.push("#D3D3D3");
+        }
+    }
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Talent Scores',
+                data: talentScores,
+                backgroundColor: bgColors,
+            },
+        ],
+    };
 
     return (
         <Layout>
             <div>
                 <div className="flex flex-wrap justify-center mt-5">
-                    <img className={"flex h-200 bg-[" + teamData.color + "] mx-20 rounded-lg"} src={teamData.logos.split(',')[0].replace('[','').replaceAll("'", '')} width="350" height="400" />
+                    <img className={"flex h-200 bg-[" + teamData.color + "] mx-20 rounded-lg"} src={teamData.logos.split(',')[0].replace('[', '').replaceAll("'", '')} width="350" height="400" />
                     <div className="flex flex-col mx-20 justify-right">
-                        <div className="m-auto flex flex-col items-left text-gray-600 text-s">
+                        <div className="m-auto flex flex-col items-left text-white text-s">
                             <div className="flex items-end">
                                 <h3 className="text-xl">School:</h3>
                                 <h3 className="text-2xl ml-14"><b>{teamData.school}</b></h3>
@@ -50,17 +150,39 @@ const TeamDetails = ({ teamData }) => {
                                 <h3 className="text-xl">Conference: </h3>
                                 <h3 className="text-2xl ml-3"><b>{teamData.conference}</b></h3>
                             </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Record: </h3>
+                                <h3 className="text-2xl ml-3"><b>{teamData.wins + '-' + teamData.losses}</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Expected Wins: </h3>
+                                <h3 className="text-2xl ml-3"><b>{teamData.expecWins}</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Recruiting Rank: </h3>
+                                <h3 className="text-2xl ml-3"><b>{teamData.recRank}</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Overall Offensive Score: </h3>
+                                <h3 className="text-2xl ml-3"><b>{teamData.overOff}</b></h3>
+                            </div>
+                            <div className="flex items-end">
+                                <h3 className="text-xl">Overall Defensive Score: </h3>
+                                <h3 className="text-2xl ml-3"><b>{teamData.overDeff}</b></h3>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-center items-center mt-20">
+                {/* <div className="flex justify-center items-center mt-20">
                     <button className="hover:text-white hover:bg-gray-600 lg:text-2xl bg-gray-300 transition duration-150 ease-in-out focus:outline-white focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700 focus:ring-white rounded border border-white text-black px-4 sm:px-20 py-1 sm:py-3 text-sm">Team Data 1</button>
                     <button className="hover:text-white hover:bg-gray-600 lg:text-2xl bg-gray-300 transition duration-150 ease-in-out focus:outline-white focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700 focus:ring-white rounded border border-white text-black px-4 sm:px-20 py-1 sm:py-3 text-sm ml-6 sm:ml-10">Team Data 2</button>
-                </div>
-                <div>
+                </div> */}
+                {/* <div>
                     <h1 className="mt-10 flex justify-center text-3xl font-bold">Charts go here</h1>
+                </div> */}
+                <div className='mx-96'>
+                    <Bar options={options} data={data} />
                 </div>
-                <div className="flex m-auto mt-20 w-3/4 rounded h-96 bg-blue-100 " />
             </div>
         </Layout>
     );
