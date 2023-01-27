9fe096bdd7fcae49e0e45a9074b38f4bb246a046
masongreen30 <73558998+masongreen30@users.noreply.github.com>
2023-01-26 22:08:37 -0500

    Update PlayersDropdown.js
    Syntax errors

--- components/PlayersDropdown.js
+++ components/PlayersDropdown.js
@@ -20,8 +20,8 @@ export default function PlayersDropdown({ teamArray, title }) {
         <Menu as='div' className='flex justify-center text-white text-lg'>
             <Menu.Button className='flex items-center px-4 py-2 rounded-md bg-gray-700 hover:bg-gray-600 active:bg-gray-500'>
                 <div className='mr-2'>{title}</div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-4">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
             </Menu.Button>
             <Menu.Items className='absolute mt-12 h-48 overflow-y-scroll scrollbar rounded-md bg-gray-700'>
