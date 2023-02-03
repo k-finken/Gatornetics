1e774408d1bd1f5a5054fbb48e36f0b520f03746
masongreen30 <73558998+masongreen30@users.noreply.github.com>
2023-02-03 10:29:22 -0500

    Update PlayersDropdown.js

--- components/PlayersDropdown.js
+++ components/PlayersDropdown.js
@@ -1,31 +1,47 @@
-import { React, forwardRef } from 'react';
+import { React, useState } from 'react';
 import Link from 'next/link';
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, Combobox } from '@headlessui/react'
 
 export default function PlayersDropdown({ teamArray, title }) {
+    const [inputPlayer, setInputPlayer] = useState("")
+    const [query, setQuery] = useState('')
+
+    const filteredPlayers = 
+        query == '' ? teamArray : teamArray.filter((player) => {
+            return (player.firstName.toLowerCase() + " " + player.lastName.toLowerCase()).includes(query.toLowerCase())
+        })
+
+    const handleChange = (e) => {
+        setInputPlayer(e.target.value);
+        setQuery(e.target.value);
+    };
+    const resetInputField = () => {
+        setInputPlayer('');
+        setQuery('');
+    };
 
     return (
-        
-        <Menu as='div' className='flex justify-center text-white text-lg'>
-            <Menu.Button className='flex items-center px-4 py-2 rounded-md bg-gray-700 hover:bg-gray-600 active:bg-gray-500'>
-                <div className='mr-2'>{title}</div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-4">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                </svg>
-            </Menu.Button>
-            <Menu.Items className='absolute mt-12 h-48 overflow-y-scroll scrollbar rounded-md bg-gray-700'>
-                <ul>
-                    { teamArray.map((player) => (
-                        <Menu.Item as='li' key={player}>
-                            {({ active }) => (
-                                <Link href={'/players/' + player.id.toString()}>
-                                    <div className='m-1 px-4 py-1 rounded-md ui-active:bg-gray-500 ui-active:cursor-pointer'>{player.firstName} {player.lastName}</div>
-                                </Link>
-                            )}
-                        </Menu.Item>
-                    )) }
-                </ul>
-            </Menu.Items>
-        </Menu>
+        <Combobox as='div' className='flex justify-center text-white text-lg'>
+            <div className='flex focus:outline-none rounded-lg bg-gray-700'>
+                <Combobox.Input placeholder={title} className='outline-none ml-2 rounded-lg bg-gray-700' onFocus={resetInputField} onChange={handleChange} />
+                <Combobox.Button className="flex items-center px-4 py-2 rounded-md">
+                    <div className='min-w-[25px] my-auto mr-5'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg></div>
+                </Combobox.Button>
+            </div>
+            <Combobox.Options className='absolute mt-12 max-h-48 overflow-y-scroll scrollbar rounded-md bg-gray-700'>
+                {filteredPlayers.length == 0 && query !== '' ? (
+                    <div className="m-1 w-64 px-4 py-1 rounded-md">Player not found.</div>
+                ) : (
+                    filteredPlayers.map((player) => (
+                    <Combobox.Option key={player.id} value={player}>
+                        <Link href={'/players/' + player.id.toString()}>
+                            <div className='m-1 w-64 px-4 py-1 rounded-md ui-active:bg-gray-500 ui-active:cursor-pointer'>{player.firstName} {player.lastName}</div>
+                        </Link>
+                    </Combobox.Option>
+                )))}
+            </Combobox.Options>
+        </Combobox>
     )
   }
