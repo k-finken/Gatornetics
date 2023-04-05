a452c0d93923f4a56a875bb293a665f5b00876b4
masongreen30 <73558998+masongreen30@users.noreply.github.com>
2023-04-05 16:26:09 -0400

    Fixed compare
    FIXED: The top graph was not showing both stats even if both players had the stats needed.
    
    UPDATED: Set up a full text search based on position selected for player 1/2. Removed the "Generate Players" button and now just default to comparing players.

--- pages/api/playersAtPosTeam.js
+++ pages/api/playersAtPosTeam.js
@@ -2,7 +2,6 @@ import prisma from '../../lib/prisma'
 
 export default async function handle(req, res) {
     const posTeam = req.query.queryPosTeam
-    console.log(posTeam)
     const posTeamTokens = posTeam.split(" ")
     const pos = posTeamTokens[0]
     const team = parseInt(posTeamTokens[1])
